94ab157fb63e5b50b81b1d1f796755647c353e37
danvargg <devg28@gmail.com>
2023-10-30 17:53:39 -0400

    initial commit for scripting.

---
+++ app/src/main/java/com/example/coding/code.kt
@@ -0,0 +1,15 @@
+package com.example.coding
+
+fun main() {
+    println("Is it going to rain?")
+    val probability = 70
+    if (probability <= 40) {
+        println("Unlikely")
+    } else if (probability <= 80) {
+        println("Likely")
+    } else if (probability < 100) {
+        println("Yes")
+    } else {
+        println("What?")
+    }
+}
